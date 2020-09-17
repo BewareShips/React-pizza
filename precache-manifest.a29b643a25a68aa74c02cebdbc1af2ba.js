412783e74b0dd001c5ed35f41bc5aa2b23b11372
Eden PolovetS <edenpolovets@gmail.com>
2020-09-17 03:18:30 +0300

    Updates

---
+++ precache-manifest.a29b643a25a68aa74c02cebdbc1af2ba.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865a82c7ac1823cb56d379591175eb8c",
+    "url": "/react-pizza/index.html"
+  },
+  {
+    "revision": "aa32ea51e08391d8a66a",
+    "url": "/react-pizza/static/css/main.8a1c3137.chunk.css"
+  },
+  {
+    "revision": "737399d8f88cca98f86c",
+    "url": "/react-pizza/static/js/2.501db086.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pizza/static/js/2.501db086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa32ea51e08391d8a66a",
+    "url": "/react-pizza/static/js/main.9893e80b.chunk.js"
+  },
+  {
+    "revision": "e037c03807b3937924e7",
+    "url": "/react-pizza/static/js/runtime-main.3c916112.js"
+  },
+  {
+    "revision": "c0e82d187b259d7f3136e1140d3e3654",
+    "url": "/react-pizza/static/media/busket.c0e82d18.svg"
+  },
+  {
+    "revision": "118838167f52e918fc044b97cc42e081",
+    "url": "/react-pizza/static/media/cart.11883816.svg"
+  },
+  {
+    "revision": "ceb2faf089a96bede51ccde3693d012b",
+    "url": "/react-pizza/static/media/empty-cart.ceb2faf0.png"
+  },
+  {
+    "revision": "a1b6992ac4bd4e78cf01f811308743c6",
+    "url": "/react-pizza/static/media/grey-arrow-left.a1b6992a.svg"
+  },
+  {
+    "revision": "f3327011e80a4caf08837bd844e21996",
+    "url": "/react-pizza/static/media/pizza-logo.f3327011.svg"
+  },
+  {
+    "revision": "5d7ef77b3ccc0b384d7b33cb4ba94dcd",
+    "url": "/react-pizza/static/media/trash.5d7ef77b.svg"
+  }
+]);
